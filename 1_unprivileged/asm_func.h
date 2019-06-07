5594c2c5945ec539fcc756ac215a3c481059e7ed
o1o1o0123 <chongze0126@gmail.com>
2019-06-07 16:48:53 +0800

    my homeword

--- 1_unprivileged/asm_func.h
+++ 1_unprivileged/asm_func.h
@@ -8,7 +8,7 @@ uint32_t read_psp(void);  //return the value of psp
 uint32_t read_msp(void);  //return the value of msp
 uint32_t read_ctrl(void); //return the value of control
 
-void start_user(uint32_t *task_addr, uint32_t *psp_init); //start user task at task_addr in unprivileged mode and set sp to psp with value psp_init
-void sw_priv(void);										  //try to switch to privileged mode from unprivileged by directly modifying the control register
+void start_user(uint32_t *task_addr, uint32_t *psp_init);  //start user task at task_addr in unprivileged mode and set sp to psp with value psp_init
+void sw_priv(void);  //try to switch to privileged mode from unprivileged by directly modifying the control register
 
 #endif
